e7633fdda9a0834c8dc60717179869dc52d46717
Daniel Ferrer <danfrrr13@gmail.com>
2019-11-07 16:35:59 -0500

    fixed port

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const express = require('express');
 
 const server = express();
 
+const port = process.env.PORT ? process.env.PORT : 4000;
 
-server.listen(4000, () => {
+server.listen(port, () => {
     console.log('=== server listening on port 4000 ===');
 });
 
